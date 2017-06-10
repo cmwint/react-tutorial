e4667dacf4e7e9f0e98d3997f298a102b4930ff3
Corinne Winthrop <corinne.winthrop@gmail.com>
2017-06-10 16:06:47 -0600

    Add react router and stateless functional component

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,29 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Header extends Component{
-	render(){
-		// $r also is equal to the current component
-		// $0 works for regular javacript
-		console.log(this);
-		return(
-			<header className="top">
-				<h1>
-					Catch
-					<span className="ofThe">
-						<span className="of">of</span>
-						<span className="the">the</span>
-					</span>
-					Day
-				</h1>
-				<h3 className="tagline">
-					<span>{this.props.tagline}</span>
-				</h3>
-				{/* this refers to the class, so Header */}
-				{/* props is the object that is available to us, contains all the data that I want to use */}
-				{/* tagline is the key in the props object that we can call */}
-			</header>
-		)
-	}
+// if you're just using the component to render html,
+// just use a "stateless functional component"
+	//class Header extends Component{
+	// so you don't need this ^^^^
+
+// instead do this
+// create a variable called Header and stick that in an arrow function
+
+	// or it could also be "var Header = function(){}"
+	// or it could also be "function Header(){}"
+const Header = (props) => {
+	// any props that are going to be used for this
+	// stateless function component will be passed in as (props)
+
+	// $r also is equal to the current component
+	// $0 works for regular javacript
+	// console.log(this);
+	return(
+		<header className="top">
+			<h1>
+				Catch
+				<span className="ofThe">
+					<span className="of">of</span>
+					<span className="the">the</span>
+				</span>
+				Day
+			</h1>
+			<h3 className="tagline">
+				{/* <span>{this.props.tagline}</span> */}
+				{/* after making this a stateless functional component */}
+				{/* (this) isn't bound to anything, it's not part of react */}
+
+				{/* the props will be passed in directly now */}
+
+				<span>{props.tagline}</span>
+			</h3>
+			{/* this refers to the class, so Header */}
+			{/* props is the object that is available to us, contains all the data that I want to use */}
+			{/* tagline is the key in the props object that we can call */}
+		</header>
+	)
+
 }
 
 export default Header;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -7,11 +7,15 @@ import React from 'react';
 // package calles 'react-dom'
 import { render } from 'react-dom';
 
+// routing
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 // relative path so webpack doesn't look in node_modules
 // the extension is assumed, don't need the .js at the end
 import App from './components/App';
 import StorePicker from './components/StorePicker.js';
-import registerServiceWorker from './registerServiceWorker';
+import NotFound from './components/NotFound.js';
+// import registerServiceWorker from './registerServiceWorker';
 
 // letting webpack compile everything and reload the page
 // webpack will also include the link tag to use this in index.html
@@ -19,13 +23,38 @@ import './index.css';
 // you can also include style sheets for each of your components
 
 
+// everything in react is a component
+// even the router itself is a component
+const Main = () => {
+	// stateless functional component
+	return(
+		<BrowserRouter>
+			<Switch>
+				{/* When the the url exactly matches the home/main page */}
+				{/* show the store picker component */}
+				<Route exact path="/store" component={StorePicker} />
+				<Route path="/store/:storeId" component={App} />
+				<Route component={NotFound} />
+			</Switch>
+		</BrowserRouter>
+	)
+}
+
 
 // using the render method from the react-dom package
 // first thing we pass it is what component we would like to render
 // this is JSX since it's kinda like our own tag
 // then the last thing is what DOM element it should render out to
-render(<App />, document.querySelector('#ohHello'));
+	//render(<App />, document.querySelector('#ohHello'));
 
-render(<StorePicker/>, document.getElementById('root'));
+render(<Main />, document.getElementById('root'));
 
-registerServiceWorker();
+// registerServiceWorker();
+
+
+
+///// Routing
+
+// React Router 4 is not part of react, but is the industry standard
+// show and hide components anywhere inside your app,
+// depending if you're on the page that you wish to show them on
