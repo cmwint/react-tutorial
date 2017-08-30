90867cc180eb12e7f9c33291a513a6433f5658b2
Corinne Winthrop <corinne.winthrop@gmail.com>
2017-08-29 22:36:31 -0600

    Add login auth from firebase

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,7 +1,10 @@
 import Rebase from 're-base';
 
 // a base - is a connection to the firebase databse
-const firebase = require('firebase');
+const firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/database');
+
 const app = firebase.initializeApp({
 	apiKey: "AIzaSyDt5MaZVeqyujiVUD8Y3Etckgg77t3yP0k",
     authDomain: "catch-of-the-day-corinne.firebaseapp.com",
@@ -9,6 +12,10 @@ const app = firebase.initializeApp({
 });
 const base = Rebase.createClass(app.database());
 
+export const githubProvider = new firebase.auth.GithubAuthProvider();
+export const facebookProvider = new firebase.auth.FacebookAuthProvider();
+export const twitterProvider = new firebase.auth.TwitterAuthProvider();
+export const auth = firebase.auth();
 
 // so we can access this database connection from any one of our files
 // just import base when we need to work with the database, and it will be autmatically be connected
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import AddFishForm from './AddFishForm';
 import PropTypes from 'prop-types';
+import base, { auth, githubProvider, facebookProvider, twitterProvider } from '../base';
 
 class Inventory extends Component{
 	constructor() {
 		super();
 		this.renderInventory = this.renderInventory.bind(this);
 		this.handleChange = this.handleChange.bind(this);
+		this.renderLogin = this.renderLogin.bind(this);
+		this.authenticate = this.authenticate.bind(this);
+		this.authHandler = this.authHandler.bind(this);
+		this.state = {
+			uid: null,
+			owner: null
+		}
 	}
 
 	// if i use this in a method that I make up, I need to bind it to the actual component itself
@@ -24,6 +32,46 @@ class Inventory extends Component{
 		}
 		// pass that up to the update fish function 
 		this.props.updateFish(key, updatedFish);
+	}
+
+	authenticate(provider) {
+		//console.log(`Trying to log in with ${provider}`);
+		// base.authWithOAuthPopup(provider, this.authHandler);
+
+		var providerAuth;
+		if(provider === 'facebook') {
+			providerAuth = facebookProvider;
+		}else if(provider === 'github') {
+			providerAuth = githubProvider;
+		}else if(provider === 'twitter') {
+			providerAuth = twitterProvider;
+		}
+
+		// var auth = base.auth();
+
+		// var provider = new base.auth.TwitterAuthProvider();
+		auth.signInWithPopup(providerAuth) 
+		    .then((result) => {
+		      const user = result.user;
+		      this.setState({
+		        user
+		      });
+		    });
+	}
+	authHandler(err, authData) {
+		console.log(authData);
+	}
+
+	renderLogin() {
+		return (
+			<nav className="login">
+				<h2>Inventory</h2>
+				<p>Sign in to mnage your store's inventory</p>
+				<button className="github" onClick={() => this.authenticate('github')}>Log In with Github</button>
+				<button className="facebook" onClick={() => this.authenticate('facebook')}>Log In with Facebook</button>
+				<button className="twitter" onClick={() => this.authenticate('twitter')}>Log In with Twitter</button>
+			</nav>
+		)
 	}
 
 	renderInventory(key) {
@@ -45,12 +93,28 @@ class Inventory extends Component{
 		)
 	}
 	render(){
+		const logout = <button>Log out!</button>
+
+		// check if no one is logged in
+		if(!this.state.uid) {
+			return <div>{this.renderLogin()}</div>
+		}
+
+		// check if they are the current owner of the store
+		if(this.state.uid !== this.state.owner) {
+			return (
+				<div>
+					<p>Sorry, you aren't the owner of the store!</p>
+					{logout}
+				</div>
+			)
+		}
+
 		return(
 			<div>
 				<h2>Inventory</h2>
-
+				{logout}
 				{Object.keys(this.props.fishes).map(this.renderInventory)}
-
 				<AddFishForm addFish={this.props.addFish}/>
 				{/* the addFish method is available via props */}
 				<button onClick={this.props.loadSamples}>Load Sample Fishes</button>
